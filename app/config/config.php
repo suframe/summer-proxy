20e906e77d688948ddef59e9a6f7bb4b70847cf8
qianjin <330576744@qq.com>
2019-07-14 01:42:27 +0800

    修改config

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,9 @@
 <?php
+$servers = [];
+if(file_exists(__DIR__ . '/servers.php')){
+    $servers = require __DIR__ . '/servers.php';
+}
+
 return [
-    'servers' => require __DIR__ . '/servers.php',
+    'servers' => $servers,
 ];
